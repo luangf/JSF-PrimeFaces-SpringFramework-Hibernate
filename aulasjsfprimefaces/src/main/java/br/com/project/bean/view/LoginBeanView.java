21a6e16ba05435ebef686abbda49ec7d935370ae
luangf <luangf222@gmail.com>
2023-03-26 02:40:17 -0300

    Finalizando a criação do encerramento da sessão

--- aulasjsfprimefaces/src/main/java/br/com/project/bean/view/LoginBeanView.java
+++ aulasjsfprimefaces/src/main/java/br/com/project/bean/view/LoginBeanView.java
@@ -1,6 +1,9 @@
 package br.com.project.bean.view;
 
+import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.context.RequestContext;
@@ -8,21 +11,43 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import br.com.project.bean.geral.BeanManagedViewAbstract;
+import br.com.project.geral.controller.SessionController;
+import br.com.srv.interfaces.SrvLogin;
 
 @Controller
-@Scope(value = "request")
-@ManagedBean(name = "loginBeanView") // faces.bean
+@Scope(value = "request") //requestScoped
+@ManagedBean(name = "loginBeanView") // javax.faces.bean jsf
 public class LoginBeanView extends BeanManagedViewAbstract {
 
 	private static final long serialVersionUID = 1L;
 
 	private String username;
-
 	private String password;
 
-	public void invalidar(ActionEvent actionEvent) {
+	@Resource
+	private SessionController sessionController; 
+	
+	@Resource
+	private SrvLogin srvLogin;
+	
+	public void invalidar(ActionEvent actionEvent) throws Exception {
 		RequestContext context=RequestContext.getCurrentInstance();
-		context.addCallbackParam("msgRetorno", "teste");
+		FacesMessage message=null;
+		boolean loggedIn=false;
+		
+		if(srvLogin.autentico(getUsername(), getPassword())) {
+			sessionController.invalidateSession(getUsername());
+			loggedIn=true;
+		}else {
+			loggedIn=false;
+			message=new FacesMessage(FacesMessage.SEVERITY_WARN, "Acesso negado",  "Login ou senha incorretos"); //tipo erro, nome erro, detalhe erro
+		}
+		
+		if(message != null) {
+			FacesContext.getCurrentInstance().addMessage("msg", message);
+		}
+		
+		context.addCallbackParam("loggedIn", loggedIn);
 	}
 
 	public String getUsername() {
